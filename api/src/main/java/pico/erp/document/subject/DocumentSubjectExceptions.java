68a460d55cbde254ddae5a6aaf98eacbea1e1358
Ethan <kkojaeh@gmail.com>
2019-02-21 17:36:16 +0900

    예외 메세지 이름 변경

--- api/src/main/java/pico/erp/document/subject/DocumentSubjectExceptions.java
+++ api/src/main/java/pico/erp/document/subject/DocumentSubjectExceptions.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 public interface DocumentSubjectExceptions {
 
-  @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "e-document-type.not.found.exception")
+  @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "e-document-subject.not.found.exception")
   class NotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
