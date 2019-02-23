01e9af97e3cce567629db5e4de67d34d939e3b49
Ethan <kkojaeh@gmail.com>
2019-02-23 14:07:18 +0900

    구현체 없을 때 기본 구현체가 자동 등록되도록 함

---
+++ test/src/main/java/pico/erp/document/config/DocumentConfiguration.java
@@ -0,0 +1,45 @@
+package pico.erp.document.config;
+
+import java.io.ByteArrayInputStream;
+import lombok.val;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pico.erp.document.context.DocumentContextFactory;
+import pico.erp.document.context.DocumentContextFactoryImpl;
+import pico.erp.document.maker.DocumentMakerDefinition;
+import pico.erp.document.template.DocumentTemplate;
+import pico.erp.shared.Public;
+import pico.erp.shared.data.ContentInputStream;
+
+@Configuration
+public class DocumentConfiguration {
+
+  @Public
+  @Bean
+  @ConditionalOnMissingBean(DocumentContextFactory.class)
+  public DocumentContextFactory noOpDocumentContextFactory() {
+    return new DocumentContextFactoryImpl();
+  }
+
+  @Public
+  @Bean
+  @ConditionalOnMissingBean(DocumentMakerDefinition.class)
+  public DocumentMakerDefinition textDocumentMakerDefinition() {
+    return new DocumentMakerDefinition() {
+
+      @Override
+      public ContentInputStream make(String name, DocumentTemplate template) {
+        val value = template.asString();
+        return ContentInputStream.builder()
+          .name(name + ".txt")
+          .contentLength(value.length())
+          .contentType("text/plain")
+          .inputStream(new ByteArrayInputStream(value.getBytes()))
+          .build();
+      }
+
+    };
+  }
+
+}
