a1143743041d96de057854d4c6a866daaea91218
Ethan <kkojaeh@gmail.com>
2019-02-24 12:52:48 +0900

    생성자 변경

--- file/src/main/java/pico/erp/document/storage/FileSystemDocumentStorageStrategy.java
+++ file/src/main/java/pico/erp/document/storage/FileSystemDocumentStorageStrategy.java
@@ -4,27 +4,31 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Value;
 import pico.erp.document.DocumentInfo;
 
 public class FileSystemDocumentStorageStrategy implements DocumentStorageStrategy {
 
-  @Setter
-  @Value("${document.storage.root-dir}")
-  private File rootDir;
+  private final File rootDirectory;
+
+  public FileSystemDocumentStorageStrategy(Config config) {
+    rootDirectory = config.rootDirectory;
+  }
 
   @Override
   public boolean exists(DocumentStorageKey storageKey) {
-    return new File(rootDir, storageKey.getValue()).exists();
+    return new File(rootDirectory, storageKey.getValue()).exists();
   }
 
   @Override
   @SneakyThrows
   public InputStream load(DocumentStorageKey storageKey) {
-    File file = new File(rootDir, storageKey.getValue());
+    File file = new File(rootDirectory, storageKey.getValue());
     if (!file.exists()) {
       throw new RuntimeException(new FileNotFoundException());
     } else {
@@ -34,7 +38,7 @@ public class FileSystemDocumentStorageStrategy implements DocumentStorageStrateg
 
   @Override
   public void remove(DocumentStorageKey storageKey) {
-    File file = new File(rootDir, storageKey.getValue());
+    File file = new File(rootDirectory, storageKey.getValue());
     if (!file.exists()) {
       throw new RuntimeException(new FileNotFoundException());
     } else {
@@ -45,8 +49,18 @@ public class FileSystemDocumentStorageStrategy implements DocumentStorageStrateg
   @Override
   @SneakyThrows
   public DocumentStorageKey save(DocumentInfo info, InputStream inputStream) {
-    File destFile = new File(rootDir, info.getId().getValue().toString());
+    File destFile = new File(rootDirectory, info.getId().getValue().toString());
     FileUtils.copyInputStreamToFile(inputStream, destFile);
-    return DocumentStorageKey.from(rootDir.toURI().relativize(destFile.toURI()).toString());
+    return DocumentStorageKey.from(rootDirectory.toURI().relativize(destFile.toURI()).toString());
+  }
+
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  @Data
+  public static class Config {
+
+    private File rootDirectory;
+
   }
 }
