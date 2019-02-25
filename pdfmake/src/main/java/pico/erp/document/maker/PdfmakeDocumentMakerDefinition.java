14aea403a8a674c9a0b0ae8ba3ac54d051d4b899
Ethan <kkojaeh@gmail.com>
2019-02-25 12:25:39 +0900

    작업파일 삭제 추가

--- pdfmake/src/main/java/pico/erp/document/maker/PdfmakeDocumentMakerDefinition.java
+++ pdfmake/src/main/java/pico/erp/document/maker/PdfmakeDocumentMakerDefinition.java
@@ -30,19 +30,24 @@ public class PdfmakeDocumentMakerDefinition implements DocumentMakerDefinition {
   @SneakyThrows
   @Override
   public ContentInputStream make(String name, DocumentTemplate template) {
-    String uuid = UUID.randomUUID().toString();
-    String input = uuid + ".js";
-    String output = uuid + ".pdf";
+    val uuid = UUID.randomUUID().toString();
+    val input = uuid + ".js";
+    val output = uuid + ".pdf";
+    val inputFile = new File(workspace, input);
+    val outputFile = new File(workspace, output);
     // file 이동
-    FileUtils.writeStringToFile(new File(workspace, input), template.asString(), "UTF-8");
+    FileUtils.writeStringToFile(inputFile, template.asString(), "UTF-8");
     run("node", "index.js", input, output);
-    byte[] bytes = FileUtils.readFileToByteArray(new File(workspace, output));
-    return ContentInputStream.builder()
+    byte[] bytes = FileUtils.readFileToByteArray(outputFile);
+    val result = ContentInputStream.builder()
       .name(name + ".pdf")
       .inputStream(new ByteArrayInputStream(bytes))
       .contentType("application/pdf")
       .contentLength(bytes.length)
       .build();
+    inputFile.delete();
+    outputFile.delete();
+    return result;
   }
 
   @SneakyThrows
